0df196931964a254a221406b9cc173417ae67ecc
CodyC1998 <cody.collins@keyin.com>
2025-02-20 12:14:13 -0330

    working medication class

---
+++ testmedication.java
@@ -0,0 +1,42 @@
+import java.util.Date;
+
+public class testmedication {
+    public static void main(String[] args) {
+        // Create a Medication object
+        medication med1 = new medication(1, "Ibuprofen", "200mg", 50, null);
+        medication med2 = new medication(2, "Amoxicillin", "500mg", 30, null);
+
+        // Display the details of each medication
+        System.out.println("\nMedication 1:");
+        System.out.println("ID: " + med1.getMedID());
+        System.out.println("Name: " + med1.getMedName());
+        System.out.println("Dosage: " + med1.getDose());
+        System.out.println("Quantity: " + med1.getQuantity());
+        System.out.println("Expiry Date: " + med1.getExpiryDate());
+        System.out.println();
+
+        System.out.println("\nMedication 2:");
+        System.out.println("ID: " + med2.getMedID());
+        System.out.println("Name: " + med2.getMedName());
+        System.out.println("Dosage: " + med2.getDose());
+        System.out.println("Quantity: " + med2.getQuantity());
+        System.out.println("Expiry Date: " + med2.getExpiryDate());
+        System.out.println();
+
+        // edit details of med 1
+        med1.setMedID(3);
+        med1.setMedName("Adderall");
+        med1.setDose("300mg");
+        med1.setQuantity(25);
+        med1.setExpiryDate(new Date());
+
+        //print edited medication
+        System.out.println("\nEdited Medication 1:");
+        System.out.println("ID: " + med1.getMedID());
+        System.out.println("Name: " + med1.getMedName());
+        System.out.println("Dosage: " + med1.getDose());
+        System.out.println("Quantity: " + med1.getQuantity());
+        System.out.println("Expiry Date: " + med1.getExpiryDate());
+        System.out.println();
+    }
+}
